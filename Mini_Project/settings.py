4e7bf79fcff4650905324b19e3524af9b0870e1a
Shivam118 <Sharmashushant118@gmail.com>
2022-06-23 17:17:40 +0530

    Static changes

--- Mini_Project/settings.py
+++ Mini_Project/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
+
 import django_heroku
 django_heroku.settings(locals())
