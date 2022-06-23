404cfbb23df3f30009e11d4c5963b560db7c6149
Shivam118 <Sharmashushant118@gmail.com>
2022-06-23 21:58:49 +0530

    StaticFiles Added

---
+++ staticfiles/JS/Data.d8f3af8ea4cc.js
@@ -0,0 +1,121 @@
+myData = [
+[41.721808,-87.7326182 , 'Advocate Christ Medical Center , 95th St Oak Lawn, IL 60453 USA '],
+[41.8959274,-87.6209988 , 'Ann and Robert H. Lurie Children's Hospital ,  Chicago, IL 60611 USA'],
+[44.9659821,-89.6678126 , 'Aspirus Heart & Lung Surgery , 425 Pine Ridge Blvd Suite 209 Wausau, WI 54401 United States '],
+[42.989578,-87.950789 , ' Aurora St Luke's Medical Center , Oklahoma Ave Milwaukee, WI 53215 United States '],
+[42.1215649,-72.6015469, 'Baystate Health , 2 Medical Center Dr STE 503 Springfield, MA 01107 United States '],
+[33.4646273,-112.0588151, 'Banner - University Medical Center Phoenix , McDowell Rd Phoenix, AZ 85006 United States '],
+[38.6370357,-90.2643583 , 'Barnes-Jewish Hospital , 1 Barnes Jewish Hospital Plaza St. Louis, MO 63110 United States'],
+[32.7878689,-96.7811212 , 'Baylor Scott & White Heart and Vascular Hospital ,Hall St Dallas, TX 75226 United States'],
+[42.5145386,-83.1926244 , 'Beaumont Hospital ,13 Mile Rd Royal Oak, MI 48073 United States '],
+[44.5021135,-88.0116076 , ' Bellin Hospital ,  Webster Ave Green Bay, WI 54301 United States '],
+[42.3374482,-71.1047915 , 'Boston Children's Hospital , 300 Longwood Ave Boston, MA 02115 United States '],
+[42.3361091,-71.1074884 , 'Brigham And Women's Hospital ,75 Francis St Boston, MA 02115 United States '],
+[37.2513459,-79.9417001 , 'Carilion Roanoke Memorial Hospital ,1906 Belleview Ave SE Roanoke, VA 24014 United States '],
+[34.0738946,-118.3808434 , 'Cedars Sinai Medical Center ,3rd St Los Angeles, CA 90048 United States '],
+[34.0976833,-118.2899098 , 'Children's Hospital Los Angeles ,4650 Sunset Blvd Los Angeles, CA 90027 United States '],
+[39.1210456,-84.5095597 , ' The Christ Hospital , 2139 Auburn Ave Cincinnati, OH 45219 United States '],
+[41.502823,-81.621722 , ' Cleveland Clinic Main Campus , 9500 Euclid Ave Cleveland, OH 44195 United States '],
+[36.11154726,-79.72616436 , 'Cone Health Heart and Vascular Center , 1121 North Church St Entrance C Greensboro, NC 27401 United States '],
+[42.281891,-83.7271714, 'C.S. Mott Children's Hospital , Hospital Dr Ann Arbor, MI 48109 United States '],
+[36.0075052,-78.938442 , 'Duke University Hospital , 2301 Erwin Rd Durham, NC 27710 United States '],
+[37.368842,-122.0802039, 'El Camino Hospital , El Camino Hospita '],
+[33.792038,-84.3221789, 'Emory University Hospital , Emory University Hospital  '],
+[39.1398314,-84.5212982, 'Good Samaritan Hospital , 375 Dixmyth Ave Cincinnati, OH 45220 United States'],
+[40.560655,-74.3357441, 'Hackensack Meridian Health Medical Group , 343 Thornall St Edison, NJ 08837 United States'],
+[33.0148185,-96.789573, 'Baylor Scott & White The Heart Hospital ,1100 Allied Dr Plano, TX 75093 United States '],
+[33.624397,-117.9296357, 'Hoag Hospital , 1 Hoag Dr Newport Beach, CA 92663 United States'],
+[29.71072,-95.3995173, 'Houston Methodist Hospital , 6565 Fannin St Houston, TX 77030 United States'],
+[34.133943,-118.1521448, 'Huntington Hospital ,California Blvd Pasadena, CA 91105 United States'],
+[38.8573966,-77.2281251, 'Inova Fairfax Hospital , 3300 Gallows Rd Falls Church, VA 22042 United States'],
+[39.7903409,-86.1633559, ' IU Health Methodist Hospital ,Senate Ave Indianapolis, IN 46202 United States'],
+[39.2967385,-76.5927362 , 'The Johns Hopkins Hospital , 1800 Orleans St Baltimore, MD 21287 United States'],
+[37.9827644,-122.0420578, 'John Muir Health, Concord Medical Center ,2540 East St Concord, CA 94520, United States'],
+[37.8251688,-122.2584626, 'Kaiser Permanente , 3701 Broadway Oakland, CA 94611 United States'],
+[34.0625852,-118.2024793, 'Keck Medicine of USC - USC Orthopaedic Surgery , 1520 San Pablo St suite 2000 Los Angeles, CA 90033 United States'],
+[39.9881975,-75.2625495, 'Lankenau Medical Center ,100 E Lancaster Ave Wynnewood, PA 19096 United States'],
+[40.5667553,-75.5241629, 'Lehigh Valley Hospital ,1200 S Cedar Crest Blvd Allentown, PA 18103 United States'],
+[41.8585414,-87.8351014, 'Loyola University Medical Center , 2160 S 1st Ave Maywood, IL 60153 United States'],
+[42.3626109,-71.067566, 'Massachusetts General Hospital , 55 Fruit St Boston, MA 02114 United States'],
+[44.0226255,-92.4668833, 'Mayo Clinic , 200 1st St SW Rochester, MN 55905 United States'],
+[38.9290664,-77.0145295, 'MedStar Washington Hospital Center , 110 Irving St NW Washington, DC 20010 United States'],
+[29.7151107,-95.3971455, 'Memorial Hermann Cancer Center - Texas Medical Center ,6400 Fannin St #2900 Houston, TX 77030 United States'],
+[33.8079396,-118.1871508, 'MemorialCare Long Beach Medical Center, 2801 Atlantic Ave Long Beach, CA 90806 United States'],
+[45.183054,-93.3689982, 'Mercy Hospital ,4050 Coon Rapids Blvd NW Coon Rapids, MN 55433 United States'],
+[39.7454533,-84.1856087, 'Miami Valley Hospital, 1 Wyoming St Dayton, OH 45409 United States'],
+[42.2839143,-83.7332766, 'University of Michigan Hospital , 1500 E Medical Center Dr Ann Arbor, MI 48109 United States'],
+[44.9525623,-93.2620166, 'Minneapolis Heart Institute® - Abbott Northwestern Hospital ,  28th St Minneapolis, MN 55407 United States'],
+[40.8810098,-73.8805517, 'Montefiore Health System , the Bronx, NY 10467 United States'],
+[40.7894006,-74.4656519, 'Morristown Medical Center , 100 Madison Ave Morristown, NJ 07960 United States'],
+[33.5616156,-117.6649857, 'Mission Hospital , 27700 Medical Center Rd Mission Viejo, CA 92691 United States'],
+[32.7459517,-79.9694931, 'MUSC Health Primary Care , 650 Ellis Oak Ave Charleston, SC 29412 United States'],
+[40.789241,-73.9545211, 'Mount Sinai Medical Center , 1 Gustave L. Levy Pl New York, NY 10029 United States'],
+[41.2565514,-95.9770385, 'Nebraska Medicine Nebraska Medical Center , 4350 Dewey Ave Omaha, NE 68105 United States'],
+[40.7643076,-73.9542869, 'NewYork-Presbyterian / Weill Cornell Medical Center , 68th St New York, NY 10065 United States'],
+[42.0640773,-87.6848502, 'NorthShore University HealthSystem , 1000 Central St #800 Evanston, IL 60201 United States'],
+[41.8950365,-87.621535, 'Northwestern Memorial Hospital , Huron St Chicago, IL 60611 United States'],
+[40.7420634,-73.9739948, 'NYU Langone Health, 550 1st Avenue New York, NY 10016 United States'],
+[35.6080415,-97.59719, 'Oklahoma Heart Hospital ,  Memorial Rd Oklahoma City, OK 73120 United States'],
+[39.9946135,-83.0173843, 'The Ohio State University Wexner Medical Center , 10th Ave Columbus, OH 43210 United States'],
+[45.4990685,-122.686773, 'OHSU Hospital , 3181 SW Sam Jackson Park Rd Portland, OR 97239 United States'],
+[39.9523924,-75.1658438, 'Penn Medicine , West Tower, Centre Square 1500 Market St Philadelphia, PA 19102 United States'],
+[40.2643934,-76.6745953, 'Penn State Health Milton S. Hershey Medical Center , 500 University Dr Hershey, PA 17033 United States'],
+[40.4952121,-74.450131, 'Robert Wood Johnson University Hospital , 1 Robert Wood Johnson Place New Brunswick, NJ 08901 United States'],
+[41.6730403,-83.5949906, 'ProMedica Toledo Hospital, Cove Blvd Toledo, OH 43606 United States'],
+[41.8747917,-87.6674637, 'Rush University Medical Center , 1620 W Harrison St Chicago, IL 60612 United States'],
+[43.5351478,-96.7425075, 'Sanford USD Medical Center and Hospital , 1305 W 18th St Sioux Falls, SD 57117 United States'],
+[32.8875768,-117.2225064, 'Scripps Health Corporate Office , 10140 Campus Point Dr San Diego, CA 92121 United States'],
+[36.8628027,-76.3044889, 'Sentara Heart Hospital , 600 Gresham Dr Norfolk, VA 23507 United States'],
+[42.9697737,-85.6649127, 'Spectrum Health Butterworth Hospital , 100 Michigan St NE Grand Rapids, MI 49503 United States'],
+[45.5744327,-94.169248, 'CentraCare - St. Cloud Hospital , 1406 6th Ave N St Cloud, MN 56303 United States'],
+[40.8046832,-73.6702617, 'St. Francis Hospital, The Heart Center® , 100 Port Washington Blvd Roslyn, NY 11576 United States'],
+[39.0486635,-94.5907941, 'Saint Luke's Hospital, 4401 Wornall Rd Kansas City, MO 64111 United States'],
+[36.1287294,-86.8439945, 'Saint Thomas West Hospital, 4220 Harding Pike Nashville, TN 37205 United States'],
+[39.939337,-86.1592051, 'St.Vincent Heart Center , 10580 N Meridian St Indianapolis, IN 46290 United States'],
+[38.5713948,-121.4694362, 'Sutter Medical Center , 2825 Capitol Ave Sacramento, CA 95816 United States '],
+[27.9378917,-82.4596206, 'Tampa General Hospital , 1 Tampa General Cir Tampa, FL 33606 United States'],
+[29.7080052,-95.4016738, 'Texas Children's Hospital , 6621 Fannin St Houston, TX 77030 United States'],
+[29.7077057,-95.4006224, 'CHI St. Luke's Health - Baylor St. Luke's Medical Cente , 6720 Bertner Ave Houston, TX 77030 United States'],
+[39.948818,-75.1571901, 'Thomas Jefferson University Hospital , 111 S 11th St Philadelphia, PA 19107 United States'],
+[33.5053811,-86.8026205, 'UAB Hospital , 1802 6th Avenue Street South Birmingham, AL 35233 United States'],
+[32.7544994,-117.165904, 'UC San Diego Health , 200 W Arbor Dr San Diego, CA 92103 United States '],
+[34.0652918,-118.4466512, 'Ronald Reagan UCLA Medical Center ,757 Westwood Plaza Los Angeles, CA 90095 United States'],
+[37.7631182,-122.4578207, 'UCSF Medical Center , 505 Parnassus Ave San Francisco, CA 94143 United States'],
+[41.5052505,-81.6060645, 'UH Cleveland Medical Center , 11100 Euclid Ave Cleveland, OH 44106 United States'],
+[
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+ 
+
+
+
+
+ 
+
+];
