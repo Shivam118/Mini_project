5e350d394598624a5d5044412aff7bead009831f
Shivam118 <Sharmashushant118@gmail.com>
2022-06-23 16:00:40 +0530

    Remove a hardcoded Django Secret key

--- Mini_Project/settings.py
+++ Mini_Project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b662&0boad)r^0y2-nr)ku48u%!%wnn)0*%4^ortd_pq^^c415'
+# SECRET_KEY = 'b662&0boad)r^0y2-nr)ku48u%!%wnn)0*%4^ortd_pq^^c415'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
