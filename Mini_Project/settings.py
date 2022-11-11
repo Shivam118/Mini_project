1728613619f5e5b373eaa146bb3100c6ed11bf05
Shivam Sharma <sharmashushant118@gmail.com>
2022-11-11 21:10:48 +0530

    Portal updated for Azure

--- Mini_Project/settings.py
+++ Mini_Project/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
